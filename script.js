ffd78acb3be80a3b2d53db3f089806ff8ada30ee
Samuel Ramos <samuelvanguarda@gmail.com>
2024-06-21 15:21:51 -0300

    FIX: the semaphore loop does not accumulates anymore and stops in each new event

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 const semaphoreImg = document.getElementById('img');
 const buttons = document.getElementById('buttons');
 let colorIndex = 0;
+let intervalId = null;
 
 const turnOn = {
   'red': () => img.src = './img/vermelho.png',
   'yellow': () => img.src = './img/amarelo.png',
   'green': () => img.src = './img/verde.png',
-  'automatic': () => setInterval(semaphoreLoop, 2000)
+  'automatic': () => intervalId = setInterval(semaphoreLoop, 2000)
+}
+
+const stopSemaphoreLoop = () => {
+  clearInterval(intervalId)
 }
 
 const semaphore = (event) => {
   const buttonClicked = event.target.id;
+  stopSemaphoreLoop();
   turnOn[buttonClicked]();
 }
 
