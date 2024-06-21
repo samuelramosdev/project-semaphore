2fc35318949a20845d8d1012a80265c6a632aa15
Samuel Ramos <samuelvanguarda@gmail.com>
2024-06-21 15:09:57 -0300

    FEAT: creates an event that makes the semaphore runs automatically

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const semaphoreImg = document.getElementById('img');
 const buttons = document.getElementById('buttons');
+let colorIndex = 0;
 
 const turnOn = {
   'red': () => img.src = './img/vermelho.png',
   'yellow': () => img.src = './img/amarelo.png',
   'green': () => img.src = './img/verde.png',
+  'automatic': () => setInterval(semaphoreLoop, 2000)
 }
 
 const semaphore = (event) => {
   const buttonClicked = event.target.id;
   turnOn[buttonClicked]();
+}
+
+const nextIndex = () => colorIndex < 2 ? colorIndex++ : colorIndex = 0;
+
+const semaphoreLoop = () => {
+  const colors = ['red', 'yellow', 'green'];
+  const color = colors[colorIndex];
+  turnOn[color]();
+  nextIndex()
 }
 
 buttons.addEventListener('click', semaphore);
